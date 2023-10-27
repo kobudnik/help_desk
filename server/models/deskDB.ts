fff8232530918545c595c8f3dbf277dafe0730d1
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-10-27 09:43:09 -0400

    Initializing & connecting to db

---
+++ server/models/deskDB.ts
@@ -0,0 +1,25 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const host = process.env.POSTGRES_HOST || 'localhost';
+const user = process.env.POSTGRES_USER || 'postgres';
+const password = process.env.POSTGRES_PASS || 'postgres';
+const database = process.env.POSTGRES_NAME || 'db_desk';
+const port = Number(process.env.POSTGRES_PORT) || 5433;
+
+const pool = new Pool({
+  host,
+  user,
+  password,
+  database,
+  port,
+});
+
+export default {
+  query: async (text: string, params?: any, callback?: any): Promise<any> => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
---
+++ server/models/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tickets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    status VARCHAR(20) DEFAULT 'new' CHECK (status IN ('new', 'in progress', 'resolved')),
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);
