114af22c80a5b3484a592b2a2f75d0fc80fa0984
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-10-27 23:40:50 -0400

    Restructured divs for easier aligning/centering

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,18 +3,18 @@ import { TicketForm } from './TicketForm';
 function App() {
   return (
     <>
-      <div className="w-full bg-yellow-100">
-        <p className="font-2xl block text-center pt-16 font-extrabold text-gray-500 mb-2">
-          {' '}
-          <span className="text-3xl mb-4  text-gray-500 mr-11 pr-11">
+      <div className="w-full bg-yellow-100 h-screen">
+        <div className="flex flex-col justify-center items-center bg-yellow-100">
+          <p className="font-2xl  pt-16 font-extrabold text-gray-500 mb-2">
             {' '}
-            Having Trouble?
-          </span>
-          <span className="text-pink-500 block text-5xl mr-28">
-            Submit a Request
-          </span>
-        </p>
-        <div className="flex justify-center  w-full bg-yellow-100 h-screen">
+            <span className="text-3xl block pl-20 text-gray-500">
+              {' '}
+              Having Trouble?
+            </span>
+            <span className="text-pink-500 block text-5xl ">
+              Submit a Request
+            </span>
+          </p>
           <TicketForm />{' '}
         </div>
       </div>
