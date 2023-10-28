7f0f2ead6f4d5d5c89b84b9c7bec1b6261c949ca
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-10-27 22:57:10 -0400

    Slight UI changes before beginning admin center

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,12 +3,15 @@ import { TicketForm } from './TicketForm';
 function App() {
   return (
     <>
-      <div className="w-full bg-yellow-100 h-screen">
-        <p className="font-2xl block text-center pt-12 font-extrabold text-gray-500 text-5xl mb-2">
+      <div className="w-full bg-yellow-100">
+        <p className="font-2xl block text-center pt-16 font-extrabold text-gray-500 mb-2">
           {' '}
-          <span className="text-pink-500">
+          <span className="text-3xl mb-4  text-gray-500 mr-11 pr-11">
             {' '}
-            Having Trouble? Submit a Request{' '}
+            Having Trouble?
+          </span>
+          <span className="text-pink-500 block text-5xl mr-28">
+            Submit a Request
           </span>
         </p>
         <div className="flex justify-center  w-full bg-yellow-100 h-screen">
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from './Navbar';
 import { Outlet } from 'react-router';
 function Layout() {
   return (
-    <div className=" h-screen">
+    <div className=" h-screen overflow-hidden">
       <Navbar />
       <Outlet />
     </div>
