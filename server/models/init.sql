f04fbca098cebe08f0017ace38cff1a6a49fba60
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-10-28 07:10:37 -0400

    db schema constraints. compose down script

--- server/models/init.sql
+++ server/models/init.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS tickets (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+ id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL CHECK (LENGTH(name) >= 3),
+    email VARCHAR(255) NOT NULL CHECK (LENGTH(email) >= 5),
     status VARCHAR(20) DEFAULT 'new' CHECK (status IN ('new', 'in progress', 'resolved')),
-    subject VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL
+    subject VARCHAR(255) NOT NULL CHECK (LENGTH(subject) >= 3),
+    description TEXT NOT NULL CHECK (LENGTH(description) >= 5),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
