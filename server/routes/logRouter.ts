92f50c566e185071797b7ec941a8923640a3e143
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-11-13 14:44:32 -0500

    Add return statements

--- server/routes/logRouter.ts
+++ server/routes/logRouter.ts
@@ -3,10 +3,10 @@ import { logController } from '../controllers/logController';
 const logRouter = Router();
 
 logRouter.get('/', logController.getInfo, (req, res) => {
-  res.status(200).json(res.locals.logEntries);
+  return res.status(200).json(res.locals.logEntries);
 });
 logRouter.get('/errors', logController.getErrors, (req, res) => {
-  res.status(200).json(res.locals.errorEntries);
+  return res.status(200).json(res.locals.errorEntries);
 });
 
 export { logRouter };
